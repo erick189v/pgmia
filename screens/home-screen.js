0e227294cc19033b84185a7a4e1df08fd2c6a3e3
Eraclio Vega <eracliovega@Eraclios-MBP.attlocal.net>
2024-01-25 11:22:38 -0500

    navigation between home screen and detail screen

--- screens/home-screen.js
+++ screens/home-screen.js
@@ -1,9 +1,13 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const HomeScreen = () => {
+    // event navigation.
+    const navigation = useNavigation()
     return (
         <View style={styles.screen}>
             <Text>This is the Home Screen</Text>
+            <Button title="Events" onPress={()=>navigation.navigate('Event')} />
         </View>
     )
 }
